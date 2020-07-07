0727a674b3791c28f5dead2fe6d37acb705d582f
ParaD0X <frostbitekarzon@gmail.com>
2020-07-07 18:16:18 +0530

    multi guild support

--- cogs/server_stats.py
+++ cogs/server_stats.py
@@ -9,6 +9,7 @@ class Stats_Cog(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
         self.change_stats.start()
+        self.ch_id={}
 
     def cog_unload(self):
         print('unloaded Stats')
@@ -18,60 +19,56 @@ class Stats_Cog(commands.Cog):
     async def on_ready(self):
         print('Ready')
 
-    @commands.Cog.listener()
-    async def on_member_join(self):
-        self.change_stats()
+    async def refresh_stats(self,g_id):
+        guild=self.bot.get_guild(id=int(g_id))
+        print(guild)
+        roles=guild.get_role(role_id=self.ch_id["guilds"][f'{g_id}']["bot_role"])
+        mem_tot_channel=self.bot.get_channel(self.ch_id["guilds"][f'{g_id}']["mem_tot_id"])
+        mem_onl_channel=self.bot.get_channel(self.ch_id["guilds"][f'{g_id}']["mem_onl_id"])
+        bot_channel=self.bot.get_channel(self.ch_id["guilds"][f'{g_id}']["bot_id"])
 
-    @commands.Cog.listener()
-    async def on_member_remove(self):
-        self.change_stats()
-
-
-    @tasks.loop(seconds=10)
-    async def change_stats(self):
         online=0
-        for member in self.guild.members:
-            if (str(member.status)=='online' or str(member.status)=='idle') and member not in self.roles.members:
+        for member in guild.members:
+            if (str(member.status)=='online' or str(member.status)=='idle') and member not in roles.members:
                 online+=1
 
         print('Got Members')
 
-        if self.mem_tot_count!=((self.guild.member_count)-(len(self.roles.members))):
-            self.mem_tot_count=(self.guild.member_count)-(len(self.roles.members))
-            await self.mem_tot_channel.edit(name=f'Total Members: {self.mem_tot_count}')
+        if self.ch_id["guilds"][f'{g_id}']["mem_tot_count"]!=((guild.member_count)-(len(roles.members))):
+            mem_tot_count=(guild.member_count)-(len(roles.members))
+            self.ch_id["guilds"][f'{g_id}']["mem_tot_count"]=mem_tot_count
+            await mem_tot_channel.edit(name=f'Total Members: {mem_tot_count}')
             print('Edited Total')
 
-        
-        if self.mem_onl_count!=online:
-            self.mem_onl_count=online
-            await self.mem_onl_channel.edit(name=f'Members Online: {self.mem_onl_count}')
+        if self.ch_id["guilds"][f'{g_id}']["mem_onl_count"]!=online:
+            mem_onl_count=online
+            self.ch_id["guilds"][f'{g_id}']["mem_onl_count"]=mem_onl_count
+            await mem_onl_channel.edit(name=f'Members Online: {mem_onl_count}')
             print('Edited Online')
 
-        
-        if self.bot_count!=len(self.roles.members):
-            self.bot_count=len(self.roles.members)
-            await self.bot_channel.edit(name=f'Total Bots: {self.bot_count}')
+        if  self.ch_id["guilds"][f'{g_id}']["bot_count"]!=len(roles.members):
+            bot_count=len(roles.members)
+            self.ch_id["guilds"][f'{g_id}']["bot_count"]=bot_count
+            await bot_channel.edit(name=f'Total Bots: {bot_count}')
             print('Edited Bots')
 
         print('DONE')
-        
+
+    @tasks.loop(seconds=10)
+    async def change_stats(self):
+        print('Bot is Online')
+        with open("stats.json",'r') as f:
+            self.ch_id=json.load(f)
+        print(f'IDs LOADED :{self.ch_id}')     
+        for guild_ids in self.ch_id["guilds"].keys():
+            print(guild_ids)
+            await self.refresh_stats(guild_ids)
+
+        with open("stats.json",'w') as f:
+            json.dump(self.ch_id,f,indent=4)
+            
     @change_stats.before_loop
     async def before_stats(self):
         print('Starting...')
         await self.bot.wait_until_ready()
-
-        self.guild=self.bot.get_guild(id=690205011497582702)
-        self.roles=self.guild.get_role(role_id=690269298685771797)
         
-        with open("stats.json",'r') as f:
-            ch_id=json.load(f)
-        print(f'IDs LOADED :{ch_id}')
-
-        self.mem_tot_channel=self.bot.get_channel(int(ch_id["mem_tot_id"]))
-        self.mem_onl_channel=self.bot.get_channel(int(ch_id["mem_onl_id"]))
-        self.bot_channel=self.bot.get_channel(int(ch_id["bot_id"]))
-
-        self.mem_tot_count=0
-        self.mem_onl_count=0
-        self.bot_count=0
-        print('Bot is Online')
--- testbot.py
+++ testbot.py
@@ -1,32 +1,125 @@
 import discord
 from discord.ext import commands, tasks
+import json
 import random
 import time
 import os
-bot_token=''
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+bot_token='NjkwMTk4MzAwNjE5ODMzNDc5.XuuhWA.o5DcKpgUxxipkZvY8pbdKpdPpNs'
 bot = commands.Bot(command_prefix = 'd:')
 @bot.event
 async def on_ready():
     print('Logging in...')
-    cog_loader()
     print('Ready')
-    await bot.change_presence(activity=discord.Game("Under Construction"))
+    cog_loader()
+    await bot.change_presence(activity=discord.Game("Kalm"))
 
 @bot.command()
+@commands.has_guild_permissions(administrator=True)
+async def stat_init(ctx):
+    bot.unload_extension(f'cogs.server_stats')
+    guild=ctx.message.guild
+    with open('stats.json','r') as f:
+        stat_json=json.load(f)
+    if str(guild.id) not in stat_json["guilds"].keys():
+        print(guild)
+        for role in guild.roles:
+            if role.name.lower()=='bots' or role.name.lower()=='bot':
+                break
+        stats1=await guild.create_voice_channel('stat1')
+        stats2=await guild.create_voice_channel('stat2')
+        stats3=await guild.create_voice_channel('stat3')
+        await ctx.send("Channels Created")
+        overwrite_perm=discord.PermissionOverwrite()
+        overwrite_perm.connect=False
+        overwrite_perm.view_channel=True
+        await stats1.set_permissions(ctx.guild.default_role,overwrite=overwrite_perm)
+        await stats2.set_permissions(ctx.guild.default_role,overwrite=overwrite_perm)
+        await stats3.set_permissions(ctx.guild.default_role,overwrite=overwrite_perm)
+        guild_stats={}
+        guild_stats["bot_role"]=role.id
+        guild_stats["mem_tot_id"]=stats1.id
+        guild_stats["mem_onl_id"]=stats2.id
+        guild_stats["bot_id"]=stats3.id
+        guild_stats["mem_tot_count"]=69
+        guild_stats["mem_onl_count"]=42
+        guild_stats["bot_count"]=0
+
+        stat_json["guilds"][f'{guild.id}']=guild_stats
+
+        with open('stats.json','w') as f:
+            json.dump(stat_json,f,indent=4)
+        print('Data Updated')
+        bot.load_extension('cogs.server_stats')    
+    
+@bot.command()
+@commands.has_guild_permissions(administrator=True)
+async def stat_remove(ctx):
+    bot.unload_extension(f'cogs.server_stats')
+    guild=ctx.message.guild
+    with open('stats.json','r') as f:
+        stat_json=json.load(f)
+    if str(guild.id) in stat_json["guilds"].keys():
+        mem_tot_channel=bot.get_channel(stat_json["guilds"][f'{guild.id}']["mem_tot_id"])
+        print(mem_tot_channel)
+        mem_onl_channel=bot.get_channel(stat_json["guilds"][f'{guild.id}']["mem_onl_id"])
+        print(mem_onl_channel)
+        bot_channel=bot.get_channel(stat_json["guilds"][f'{guild.id}']["bot_id"])
+        print(bot_channel)
+
+        del stat_json["guilds"][f'{guild.id}']
+        with open('stats.json','w') as f:
+            json.dump(stat_json,f,indent=4)
+
+        try:
+            await mem_onl_channel.delete()
+            print('onl')
+        except:
+            pass
+        try:
+            await mem_tot_channel.delete()
+            print('tot')
+        except:
+            pass
+        try:
+            await bot_channel.delete()
+            print('bot')
+        except:
+            pass
+        print('Deleted')
+        await ctx.send("Channels Deleted")
+        bot.load_extension('cogs.server_stats')
+
+@bot.command()
+@commands.is_owner()
 async def load(ctx,extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f'Extension {extension} loaded')
+
+@bot.command()
+async def invite(ctx):
+    inv_link="Invite Stonks To Your Server \n https://discord.com/api/oauth2/authorize?client_id=728878804491304980&permissions=8&scope=bot"
+    await ctx.send(inv_link)
 
 @bot.command(aliases=['latency','speed'])
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency * 1000)} ms')
 
 @bot.command()
+@commands.is_owner()
 async def unload(ctx,extension):
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f'Extension {extension} unloaded')
 
 @bot.command()
+@commands.is_owner()
 async def reload(ctx,extension):
     bot.unload_extension(f'cogs.{extension}')
     print('reloading...')
